ca75586c5598687d5eb647fb0ef35c28fa78865a
AHMET TALHA ŞAHİN <talha040441@hotmail.com>
2022-04-15 03:46:00 +0300

    Add project files.

---
+++ EmailSender/EmailSender.cs
@@ -0,0 +1,53 @@
+﻿using FluentEmail.Core;
+using FluentEmail.Smtp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSender
+{
+    public class EmailSender
+    {
+        public EmailSender()
+        {
+
+        }
+
+        public async Task<bool> SendingEmail(EmailSenderModel model)
+        {
+            try
+            {
+                //string host = "smtp.gmail.com";
+                string host = "smtp.office365.com";
+
+                var sender = new SmtpSender(() => new SmtpClient(host)
+                {
+                    UseDefaultCredentials = false,
+                    Port = 587,
+                    Credentials = new NetworkCredential(model.From, model.Password),
+                    EnableSsl = true,
+                });
+
+                Email.DefaultSender = sender;
+
+                var email = Email
+                    .From(model.From, "You Have mail from Talha")
+                    .To(model.To, "Name")
+                    .Subject("Hey there!")
+                    .Body("Hello");
+
+                var response = await email.SendAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ EmailSender/Program.cs
@@ -0,0 +1,35 @@
+﻿using FluentEmail.Core;
+using FluentEmail.Smtp;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace EmailSender
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            EmailSenderModel emailSenderModel = new EmailSenderModel();
+
+            emailSenderModel.From = "From Mail";
+            emailSenderModel.To = "To Mail";
+            emailSenderModel.Password = "Password";
+            emailSenderModel.Body = "This is the body of email";
+
+            EmailSender emailSender = new EmailSender();
+            bool result = await emailSender.SendingEmail(emailSenderModel);
+
+            if (result)
+            {
+                Console.WriteLine("Successfull!");
+            }
+            else
+            {
+                Console.WriteLine("Unsuccessful!");
+            }
+
+        }
+    }
+}
